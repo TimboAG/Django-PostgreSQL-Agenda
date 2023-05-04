26ded34bb4b0c18f7ca1664b737efa27d23669ab
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-05-04 16:12:18 -0300

    aprendiendo django

--- contactos/models.py
+++ contactos/models.py
@@ -12,3 +12,27 @@ class Persona(models.Model):
     dni = models.PositiveSmallIntegerField(max_length=8, primary_key=True)
     apellido = models.CharField(max_length=25)
     nombre = models.CharField(max_length=25)
+    sexos = [
+        ('F', 'Femenino'),
+        ('M', 'Masculino')
+    ]
+    sexo = models.CharField(max_length=1, choices=sexos, default='F')
+    fechaNacimiento = models.DateField()
+    ciudad = models.ForeignKey(
+        Ciudad, blank=False, null=False, on_delete=models.CASCADE)
+
+    def nombreCompleto(self):
+        txt = "{0}{1}"
+        return txt.format(self.apellido, self.nombre)
+
+
+class Telefono(models.Model):
+    persona = models.ForeignKey(
+        Persona, blank=False, null=False, on_delete=models.CASCADE)
+    numero = models.CharField(max_length=10)
+
+
+class Email(models.Model):
+    persona = models.ForeignKey(
+        Persona, blank=False, null=False, on_delete=models.CASCADE)
+    email = models.CharField(max_length=180)
