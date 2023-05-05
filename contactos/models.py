121a5683e0666b67e03a1f49faa678097bb019c8
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-05-05 17:18:47 -0300

    aprendiendo django

--- contactos/models.py
+++ contactos/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Ciudad(models.Model):
     codigo = models.PositiveSmallIntegerField(primary_key=True)
     nombre = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return "{0}".format(self.nombre)
 
 
 class Persona(models.Model):
@@ -22,17 +25,25 @@ class Persona(models.Model):
         Ciudad, blank=False, null=False, on_delete=models.CASCADE)
 
     def nombreCompleto(self):
-        txt = "{0}{1}"
-        return txt.format(self.apellido, self.nombre)
-
+        txt = "{0}{1}{2}"
+        return txt.format(self.apellido," ", self.nombre)
+    
+    def __str__(self) :
+        return self.nombreCompleto()
 
 class Telefono(models.Model):
     persona = models.ForeignKey(
         Persona, blank=False, null=False, on_delete=models.CASCADE)
     numero = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return "{0} ({1})".format(self.numero, self.persona)
 
 
 class Email(models.Model):
     persona = models.ForeignKey(
         Persona, blank=False, null=False, on_delete=models.CASCADE)
     email = models.CharField(max_length=180)
+    
+    def __str__(self):
+        return "{0} ({1})".format(self.email, self.persona)
